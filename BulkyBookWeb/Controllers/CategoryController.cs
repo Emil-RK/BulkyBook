1852d7ab90fcf1a3978490b11fc9ef36e667739c
Emil LASLAU <emil.laslau@indsoft.ro>
2024-06-14 10:31:44 +0300

    Edit category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -30,9 +30,56 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (obj.Name == obj.DiaplsyOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The display order can not exactly match the nane.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDB = _db.Categories.Find(id);
+           // var categoryFromDBFirst = _db.Categories.FirstOrDefault(i => i.Id == id);
+           // var categoryFromDBSingle = _db.Categories.SingleOrDefault(i => i.Id == id);
+
+            if(categoryFromDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDB);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DiaplsyOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The display order can not exactly match the nane.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
         }
     }
 }
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -8,6 +9,8 @@ namespace BulkyBookWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Diaplsy Order")]
+        [Range(1,1000,ErrorMessage ="Display order must be between 1 and 1000 only!")]
         public int DiaplsyOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
 
